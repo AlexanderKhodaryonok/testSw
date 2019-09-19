e2361e38eb9a6ae09ed8beca10d0ef8f524a9994
AlexanderKhodaryonok <alexanderkhodaryonok@gmail.com>
2019-09-19 20:39:15 +0300

    add webpack config

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import s from './App.module.scss';
+
+class App extends Component {
+
+	render() {
+		return (
+			<div className={s.head}>
+				<h1>My React App!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "index_bundle.js"
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },      
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+            }
+          },
+          { loader: 'sass-loader' }
+        ],
+      },
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      })
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
